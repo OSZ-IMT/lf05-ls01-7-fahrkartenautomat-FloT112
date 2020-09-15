f46ad0f160bddf41dce2db68292a34a06f6112ec
FloT112 <70951999+FloT112@users.noreply.github.com>
2020-09-15 15:45:07 +0200

    Update Fahrkartenautomat.java
    "anzahlTickets" deklariert als float
    
    Eingabe für festlegung der Anzahl der Tickets erstellt:
    --> System.out.print("Anzahl der Tickets: ");
            anzahlTickets = tastatur.nextFloat();
    
    Geldbetrag für ein Ticket multipliziert mit der Anzahl der Tickets:
    --> while (eingezahlterGesamtbetrag < zuZahlenderBetrag*anzahlTickets)
          float d = zuZahlenderBetrag*anzahlTickets - eingezahlterGesamtbetrag;
    
    Code für die Rechnung und die Ausgabe des Rückgeldes angepasst:
    --> rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag*anzahlTickets;
    
    Beschreibung der Operationen mit den Variablen dahinter gekennzeichnet im Code

--- src/main/java/Fahrkartenautomat.java
+++ src/main/java/Fahrkartenautomat.java
@@ -9,88 +9,84 @@ class Fahrkartenautomat {
 
     public static void main(String[] args) {
         Scanner tastatur = new Scanner(System.in);
-        // Achtung!!!!
-        // Hier wird deutlich, warum double nicht für Geldbeträge geeignet ist.
-        // =======================================
         float zuZahlenderBetrag = 0;
         float eingezahlterGesamtbetrag;
         float eingeworfeneMuenze;
         float rueckgabebetrag;
         float eingegebenerBetrag;
-        // Die Eingabe erfolgt anwenderfreudlich mit Dezimalpunkt: just testing
+        float anzahlTickets;
+        
+        // zuZahlenderbetrag, eingezahlterGesamtbetrag, eingeworfeneMuenze, rueckgabebetrag, eingegebenerBetrag und anzahlTickets sind die Variablen (d auch) mit Dateityp: float
 
 
-                // Den zu zahlenden Betrag ermittelt normalerweise der Automat
-        // aufgrund der gewählten Fahrkarte(n).
-        // -----------------------------------
+                
 
         System.out.print("Zu zahlender Betrag (Euro): ");
-        zuZahlenderBetrag = tastatur.nextFloat();
+        zuZahlenderBetrag = tastatur.nextFloat(); // Wert für "zuZahlenderBetrag" wird festgelegt durch die Eingabe
+        System.out.print("Anzahl der Tickets: ");
+        anzahlTickets = tastatur.nextFloat(); // Wert für "anzahlTickets" wird festgelegt durch die Eingabe
 
-        // Geldeinwurf
-        // -----------
-        eingezahlterGesamtbetrag = (float) 0.00;
-        while (eingezahlterGesamtbetrag < zuZahlenderBetrag) {
-            float d = zuZahlenderBetrag - eingezahlterGesamtbetrag;
+   
+        eingezahlterGesamtbetrag = (float) 0.00; // Wert wird auf Null gesetzt
+        while (eingezahlterGesamtbetrag < zuZahlenderBetrag*anzahlTickets) { // eine Schleife in der festgelegt ist, so lange der "eingezahlterGesamtbetrag" kleiner als das Produkt von "zuZahlenderBetrag" und "anzahlTickets" beginnt die Schleife immer wieder von neu
+            float d = zuZahlenderBetrag*anzahlTickets - eingezahlterGesamtbetrag; // d ist auch eine Variable mit Dateityp float // von "zuZahlenderBetrag*anzahlTickets" wir der mit der Tastatur eingetippte Betrag "eingezahlterGesamtbetrag" abgezogen und das Restgeld(wenn vorhanden = eingezahlter Betrag > gesamtBetrag) wird ausgegeben und das Ticket wird gedruckt, weil die Bedingung erfüllt ist
             System.out.printf( "%.2f\n" ,     d);
             System.out.print("Eingabe (mind. 5Ct, höchstens 2 Euro): ");
-            eingeworfeneMuenze = tastatur.nextFloat();
-            eingezahlterGesamtbetrag += eingeworfeneMuenze;
+            eingeworfeneMuenze = tastatur.nextFloat(); // Wert für das eingeworfene Geld kann festgelegt werden
+            eingezahlterGesamtbetrag += eingeworfeneMuenze; // Der Gesamtbetrag mit 0,0 der am Anfang festgelegt wurde, wird jetzt mit dem Geld/Wert addiert, welcher eingetragen wurde
         }
-        // Fahrscheinausgabe
-        // -----------------
-        System.out.println("\nFahrschein wird ausgegeben");
+       
+        
+        System.out.println("\nFahrschein");
         for (int i = 0; i < 8; i++) {
             System.out.print("=");
             try {
                 Thread.sleep(250);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
         System.out.println("\n\n");
 
-        // Rückgeldberechnung und -Ausgabe
-        // -------------------------------
-        rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
+        
+        rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag*anzahlTickets;
         if (rueckgabebetrag > 0.0) {
-            System.out.println("Der Rückgabebetrag in Höhe von " + rueckgabebetrag + " EURO");
+            System.out.println("Der Rückgabebetrag in Höhe von " + rueckgabebetrag + " EURO"); // hier wird der Rückgabebetrag angezeigt, welcher davor ausgerechnet wurde
             System.out.println("wird in folgenden Münzen ausgezahlt:");
 
-            while (rueckgabebetrag >= 2.00) // 2 EURO-Münzen
+            while (rueckgabebetrag >= 2.00) 
             {
                 System.out.println("2 Euro");
                 rueckgabebetrag -= 2.00;
             }
-            while (rueckgabebetrag >= 1.00) // 1 EURO-Münzen
+            while (rueckgabebetrag >= 1.00) 
             {
                 System.out.println("1 Euro");
                 rueckgabebetrag -= 1.00;
             }
-            while (rueckgabebetrag >= 0.50) // 50 CENT-Münzen
+            while (rueckgabebetrag >= 0.50) 
             {
                 System.out.println("50 Cent");
                 rueckgabebetrag -= 0.50;
             }
-            while (rueckgabebetrag >= 0.20) // 20 CENT-Münzen
+            while (rueckgabebetrag >= 0.20) 
             {
                 System.out.println("20 Cent");
                 rueckgabebetrag -= 0.20;
             }
-            while (rueckgabebetrag >= 0.10) // 10 CENT-Münzen
+            while (rueckgabebetrag >= 0.10) 
             {
                 System.out.println("10 Cent");
                 rueckgabebetrag -= 0.10;
             }
-            while (rueckgabebetrag >= 0.05)// 5 CENT-Münzen
+            while (rueckgabebetrag >= 0.05)
             {
                 System.out.println("5 Cent");
                 rueckgabebetrag -= 0.05;
             }
         }
 
-        System.out.println("\nVergessen Sie nicht, den Fahrschein\n" +
+        System.out.println("\nVergessen Sie nicht, den/die Fahrschein/e\n" +
                 "vor Fahrtantritt entwerten zu lassen!\n" +
                 "Wir wünschen Ihnen eine gute Fahrt.");
     }
